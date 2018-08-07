6cd5d7cb9d6856513126965e3885d477698e93f2
Ujvalsaigopal <42155978+Ujvalsaigopal@users.noreply.github.com>
2018-08-07 16:07:05 -0400

    Display By ID

--- DAOProjectDesign/src/DAO/DAOInterface.java
+++ DAOProjectDesign/src/DAO/DAOInterface.java
@@ -4,4 +4,5 @@ import Model.Employee;
 
 public interface DAOInterface {
 public Employee DAOinsert(Employee emp); 
+public String DAOfindbyID(String tempEmpID);
 }
--- DAOProjectDesign/src/Utilities/ServiceUtilities.java
+++ DAOProjectDesign/src/Utilities/ServiceUtilities.java
@@ -6,15 +6,21 @@ import Model.Employee;
 
 public class ServiceUtilities {
 	Employee emp = null;
-
+// COllecting the Employee Information from User and return as employee Object.
 	public static Employee CollectInfo_add() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Employee ID:");
-		String id = sc.nextLine();
+		String id = sc.next();
 		System.out.println("Enter Employee Name:");
-		String name = sc.nextLine();
+		String name = sc.next();
 		Employee tempEmp = new Employee(id, name);
-		sc.close();
 		return tempEmp;
+	}
+	// User input ID to the DAO.
+	public static String displayNameByID(){
+		Scanner sd = new Scanner(System.in);
+		System.out.println("ID:");
+		String tempID = sd.next();
+		return tempID;
 	}
 }
